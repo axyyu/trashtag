81d153f31f3bdf83942a63348c61626d790a1d4a
Andrew Wang <andrewxiaoyu0@gmail.com>
2019-03-17 01:06:26 -0400

    Admin finished

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,25 +1,39 @@
 var database = firebase.database();
 var eventsRef = database.ref('admin/events');
 var tagsRef = database.ref('user/tags');
+mapboxgl.accessToken =
+	'pk.eyJ1IjoiY3JvdHRtIiwiYSI6ImNqdGJ2eDFsdTBxNTY0NG9hajA5YjZkY3YifQ.yDXoV7EwWmqJuEydkMAkbQ';
 
-// Global Arrays
-var tagDict = [];
+// Mapbox Stuff
+//Secret Key sk.eyJ1IjoiY3JvdHRtIiwiYSI6ImNqdGJ3MWRxMDBxN3A0OWw2M3doZG02YmIifQ.R9F__o46cO5chcLmlllbFA
+//Public token: pk.eyJ1IjoiY3JvdHRtIiwiYSI6ImNqdGJ2eDFsdTBxNTY0NG9hajA5YjZkY3YifQ.yDXoV7EwWmqJuEydkMAkbQ
+
+// Map
 var map;
+var markers = [];
 
-// Events
+// Data
+var tagDict = {};
+var tagKeys = [];
+
 var eventsDict = {};
+var pastKeys = [];
+var currentKeys = [];
+var futureKeys = [];
 
+// Filters
+var currentFilter = true;
+var futureFilter = true;
+var pastFilter = false;
+var tagFilter = true;
+
+// Temp Var
 var tempLngLat;
-
-var geojson = {
-	type: 'FeatureCollection',
-	features: []
-};
 
 $(document).ready(() => {
 	hidePopupHandler();
-	getTags();
 	initMap();
+	getTags();
 	getEvents();
 });
 
@@ -32,73 +46,100 @@ function hidePopupHandler() {
 	});
 }
 
-// Firebase Stuff
-
-function getTags() {
-	tagsRef.on('value', function(data) {
-		tagDict = data.val();
-		for (key in tagDict) {
-			geojson['features'].push({
-				type: 'Feature',
-				geometry: {
-					type: 'Point',
-					coordinates: [
-						tagDict[key]['location']['0'],
-						tagDict[key]['location']['1']
-					]
-				}
-			});
-			var el = document.createElement('div');
-			el.className = 'marker';
-			new mapboxgl.Marker(el)
-				.setLngLat([
-					tagDict[key]['location']['0'],
-					tagDict[key]['location']['1']
-				])
-				.addTo(map);
-		}
-	});
+function updateDisplay() {
+	showFilters();
+	showMarkers();
+	showEvents();
 }
 
-function getEvents() {
-	eventsRef.on('value', function(data) {
-		eventsDict = data.val();
+// Filters
 
-		var currentDate = new Date();
-
-		for (key in eventsDict) {
-			const startDate = new Date(
-				`${eventsDict[key].date} ${eventsDict[key].start}`
-			);
-			const endDate = new Date(
-				`${eventsDict[key].date} ${eventsDict[key].end}`
-			);
-			if (endDate < currentDate) {
-				// past
-				showEvent(key);
-			} else if (startDate > currentDate) {
-				// future
-				showEvent(key);
-			} else {
-				// present
-				showEvent(key);
-			}
-		}
-	});
+function showFilters() {
+	filterClass(tagFilter, 'filter-tag');
+	filterClass(pastFilter, 'filter-past');
+	filterClass(currentFilter, 'filter-current');
+	filterClass(futureFilter, 'filter-future');
 }
 
-function showEvent(key) {
+function filterClass(filter, className) {
+	if (filter) {
+		if ($(`#filterlist .${className}`).hasClass('off'))
+			$(`#filterlist .${className}`).removeClass('off');
+	} else {
+		if (!$(`#filterlist .${className}`).hasClass('off'))
+			$(`#filterlist .${className}`).addClass('off');
+	}
+}
+
+function toggleFilter(filter) {
+	switch (filter) {
+		case 'tag':
+			tagFilter = !tagFilter;
+			break;
+		case 'past':
+			pastFilter = !pastFilter;
+			break;
+		case 'current':
+			currentFilter = !currentFilter;
+			break;
+		case 'future':
+			futureFilter = !futureFilter;
+			break;
+		default:
+			break;
+	}
+	updateDisplay();
+}
+
+// Map
+function initMap() {
+	map = new mapboxgl.Map({
+		container: 'mapcontainer',
+		center: [ -73.679246, 42.729734 ],
+		style: 'mapbox://styles/mapbox/dark-v10',
+		zoom: 16
+	});
+
+	map.addControl(new mapboxgl.NavigationControl());
+
+	map.on('contextmenu', onRightClickMap);
+}
+
+function addMarker(valDict, key, className) {
 	var el = document.createElement('div');
-	el.className = 'marker';
+	el.className = `marker ${className}`;
+	el.innerHTML = '';
 
-	new mapboxgl.Marker(el)
-		.setLngLat([
-			eventsDict[key]['location']['0'],
-			eventsDict[key]['location']['1']
-		])
-		.addTo(map);
+	let loc = [ valDict[key]['location']['0'], valDict[key]['location']['1'] ];
+
+	let m = new mapboxgl.Marker(el).setLngLat(loc).addTo(map);
+
+	$(el).off().on('click', () => {
+		map.flyTo({ center: loc });
+	});
+	markers.push(m);
 }
 
+function showMarkers() {
+	markers.forEach((m) => {
+		m.remove();
+	});
+	markers = [];
+	showMarkersType(tagFilter, tagKeys, tagDict, 'tag');
+	showMarkersType(pastFilter, pastKeys, eventsDict, 'past');
+	showMarkersType(currentFilter, currentKeys, eventsDict, 'current');
+	showMarkersType(futureFilter, futureKeys, eventsDict, 'future');
+}
+
+function showMarkersType(filter, keys, dict, className) {
+	if (filter) {
+		keys.forEach((key) => {
+			addMarker(dict, key, `marker-${className}`);
+		});
+	}
+}
+
+// Popup
 function addEvent() {
 	$('#popup').hide();
 	const date = $('#dateTimePicker').val();
@@ -113,14 +154,6 @@ function addEvent() {
 	});
 }
 
-function deleteEvent(eventId) {
-	const oldRef = eventsRef.child(eventId).remove();
-}
-// Mapbox Stuff
-//Secret Key sk.eyJ1IjoiY3JvdHRtIiwiYSI6ImNqdGJ3MWRxMDBxN3A0OWw2M3doZG02YmIifQ.R9F__o46cO5chcLmlllbFA
-//Public token: pk.eyJ1IjoiY3JvdHRtIiwiYSI6ImNqdGJ2eDFsdTBxNTY0NG9hajA5YjZkY3YifQ.yDXoV7EwWmqJuEydkMAkbQ
-
-// Popup
 function showPopup(point) {
 	const ref = {
 		getBoundingClientRect: () => ({
@@ -137,7 +170,6 @@ function showPopup(point) {
 	const pop = new Popper(ref, $('#popup'));
 	$('#popup').show();
 }
-function onClickMap(data) {}
 
 function onRightClickMap({ point, lngLat }) {
 	showPopup(point);
@@ -152,30 +184,73 @@ function onRightClickMap({ point, lngLat }) {
 	$('#location').val(`${lngLat.lng.toFixed(4)}, ${lngLat.lat.toFixed(4)}`);
 }
 
-mapboxgl.accessToken =
-	'pk.eyJ1IjoiY3JvdHRtIiwiYSI6ImNqdGJ2eDFsdTBxNTY0NG9hajA5YjZkY3YifQ.yDXoV7EwWmqJuEydkMAkbQ';
-function initMap() {
-	map = new mapboxgl.Map({
-		container: 'mapcontainer',
-		center: [ -73.679246, 42.729734 ],
-		style: 'mapbox://styles/mapbox/dark-v10',
-		zoom: 16
+// Events
+function showEvents() {
+	$('#eventsList').empty();
+	showEventsType(currentFilter, currentKeys, 'current');
+	showEventsType(futureFilter, futureKeys, 'planned');
+	showEventsType(pastFilter, pastKeys, 'ended');
+}
+function showEventsType(filter, keys, status) {
+	if (filter) {
+		keys.forEach((key) => {
+			addEventToList(eventsDict[key], status, key);
+		});
+	}
+}
+function addEventToList(curEvent, status, key) {
+	var html = `<div class='eventPanel' onclick='gotoEvent("${key}")'>
+    <label class='${status}'>${status.toUpperCase()}</label>
+    <h1>${curEvent.date}</h1>
+    <h4>${curEvent.start} - ${curEvent.end}<h4>
+    </div>`;
+	$('#eventsList').append(html);
+}
+
+function gotoEvent(key) {
+	let loc = [
+		eventsDict[key]['location']['0'],
+		eventsDict[key]['location']['1']
+	];
+	map.flyTo({ center: loc });
+}
+
+// Firebase
+function getTags() {
+	tagsRef.on('value', function(data) {
+		tagDict = data.val();
+		tagKeys = Object.keys(tagDict);
+		updateDisplay();
 	});
+}
 
-	map.addControl(new mapboxgl.NavigationControl());
+function getEvents() {
+	eventsRef.on('value', function(data) {
+		pastKeys = [];
+		currentKeys = [];
+		futureKeys = [];
 
-	map.on('click', onClickMap);
-	map.on('contextmenu', onRightClickMap);
+		eventsDict = data.val();
 
-	geojson.features.forEach(function(marker) {
-		console.log('Adding marker:', marker);
-		// create a HTML element for each feature
-		var el = document.createElement('div');
-		el.className = 'marker';
+		var currentDate = new Date();
 
-		// make a marker for each feature and add to the map
-		new mapboxgl.Marker(el)
-			.setLngLat(marker.geometry.coordinates)
-			.addTo(map);
+		for (key in eventsDict) {
+			const curEvent = eventsDict[key];
+			const startDate = new Date(`${curEvent.date} ${curEvent.start}`);
+			const endDate = new Date(`${curEvent.date} ${curEvent.end}`);
+
+			if (endDate < currentDate) {
+				// past
+				pastKeys.push(key);
+			} else if (startDate > currentDate) {
+				// future
+				futureKeys.push(key);
+			} else {
+				// present
+				currentKeys.push(key);
+			}
+		}
+
+		updateDisplay();
 	});
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,132 @@
+var map;
+var geolocate;
+var currentLocation;
+var markers = [];
+
+var confirmationState = false;
+var database = firebase.database();
+var eventsRef = database.ref('admin/events');
+var tagsRef = database.ref('user/tags');
+
+var tagDict = {};
+var tagKeys = [];
+
+var eventsDict = {};
+var currentKeys = [];
+var futureKeys = [];
+
 $(document).ready(() => {
 	initMap();
+	getTags();
+	getEvents();
 });
+
+function updateDisplay() {
+	showMarkers();
+	showEvents();
+}
+
+// Events
+function showEvents() {
+	$('#eventsList').empty();
+	showEventsType(currentKeys, 'current');
+	showEventsType(futureKeys, 'planned');
+}
+function showEventsType(keys, status) {
+	keys.forEach((key) => {
+		addEventToList(eventsDict[key], status, key);
+	});
+}
+function addEventToList(curEvent, status, key) {
+	var html = `<div class='eventPanel' onclick='gotoEvent("${key}")'>
+    <label class='${status}'>${status.toUpperCase()}</label>
+    <h1>${curEvent.date}</h1>
+    <h4>${curEvent.start} - ${curEvent.end}<h4>
+    </div>`;
+	$('#eventsList').append(html);
+}
+
+function gotoEvent(key) {
+	let loc = [
+		eventsDict[key]['location']['0'],
+		eventsDict[key]['location']['1']
+	];
+	map.flyTo({ center: loc });
+}
+
+// Markers
+
+function showMarkers() {
+	markers.forEach((m) => {
+		m.remove();
+	});
+	markers = [];
+	showMarkersType(tagKeys, tagDict, 'tag');
+	showMarkersType(currentKeys, eventsDict, 'current');
+	showMarkersType(futureKeys, eventsDict, 'future');
+}
+
+function showMarkersType(keys, dict, className) {
+	keys.forEach((key) => {
+		addMarker(dict, key, `marker-${className}`);
+	});
+}
+
+function getTags() {
+	tagsRef.on('value', function(data) {
+		tagDict = data.val();
+		tagKeys = Object.keys(tagDict);
+		updateDisplay();
+	});
+}
+
+function getEvents() {
+	eventsRef.on('value', function(data) {
+		currentKeys = [];
+		futureKeys = [];
+
+		eventsDict = data.val();
+
+		var currentDate = new Date();
+
+		for (key in eventsDict) {
+			const curEvent = eventsDict[key];
+			const startDate = new Date(`${curEvent.date} ${curEvent.start}`);
+			const endDate = new Date(`${curEvent.date} ${curEvent.end}`);
+
+			if (startDate > currentDate) {
+				// future
+				futureKeys.push(key);
+			} else if (currentDate > startDate && currentDate < endDate) {
+				// present
+				currentKeys.push(key);
+			}
+		}
+
+		updateDisplay();
+	});
+}
+
+function addMarker(valDict, key, className) {
+	var el = document.createElement('div');
+	el.className = `marker ${className}`;
+	el.innerHTML = '';
+
+	let loc = [ valDict[key]['location']['0'], valDict[key]['location']['1'] ];
+
+	let m = new mapboxgl.Marker(el).setLngLat(loc).addTo(map);
+
+	$(el).off().on('click', () => {
+		map.flyTo({ center: loc });
+	});
+	markers.push(m);
+}
+
+// When the user clicks on div, open the popup
+function popup() {
+	var popup = document.getElementById('myPopup');
+	popup.classList.toggle('show');
+}
 
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
@@ -19,12 +145,41 @@ function closeNav() {
 mapboxgl.accessToken =
 	'pk.eyJ1IjoiY3JvdHRtIiwiYSI6ImNqdGJ2eDFsdTBxNTY0NG9hajA5YjZkY3YifQ.yDXoV7EwWmqJuEydkMAkbQ';
 function initMap() {
-	var map = new mapboxgl.Map({
+	map = new mapboxgl.Map({
 		container: 'mapcontainer',
 		center: [ -73.679246, 42.729734 ],
 		style: 'mapbox://styles/mapbox/dark-v10',
 		zoom: 16
 	});
 
+	// map.addControl(new mapboxgl.NavigationControl());
+
+	geolocate = new mapboxgl.GeolocateControl({
+		positionOptions: {
+			enableHighAccuracy: true
+		},
+		trackUserLocation: true
+	});
+
 	map.addControl(new mapboxgl.NavigationControl());
+	map.addControl(geolocate);
+
+	geolocate.on('geolocate', ({ coords }) => {
+		currentLocation = [ coords.longitude, coords.latitude ];
+	});
+
+	map.on('load', () => {
+		// geolocate.trigger();
+	});
 }
+
+function report() {
+	console.log(currentLocation);
+	tagsRef.push({
+		location: currentLocation
+	});
+}
+
+$('#toggle').click(function() {
+	$('#dates').toggle();
+});
